f22466286c16477486624c3714d0dc8ffe3d6c8d
vaibhav buccha <jainvaibhav415@gmail.com>
2019-05-30 22:45:01 +0530

    Create sorthandopr.js

---
+++ sorthandopr.js
@@ -0,0 +1,12 @@
+var myapp = new Vue({
+    el : "#our-app",
+    data:{
+        name: "vaibhav buccha",
+        email: "jainvaibhav415@gmail.com",
+        title: "this is title which is dynamic",
+        image: "https://avatars3.githubusercontent.com/u/29331854?s=460&v=4",
+        isactive: true,
+        ulink : "https://github.com/vaibhavbuccha/vue.js",
+        content: "<p>this is the paragarph</p><h3>this is the heading </h3>"
+    }
+});
