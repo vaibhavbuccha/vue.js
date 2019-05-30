9ecc5671eb972e10966ede783b660bb0e603db1c
vaibhav buccha <jainvaibhav415@gmail.com>
2019-05-30 16:52:45 +0530

    Create el_and_data.js

---
+++ el_and_data.js
@@ -0,0 +1,11 @@
+// we create a instance of Vue class.
+var vue = new Vue({
+    // el is predefine which indicates element in which we pass our id on which we want controll.
+    el : "#our-app",
+    // data is also predefine in which we have to pass our data .
+    data :{
+        message : "this is vue message",
+        fname : "vaibhav buccha",
+        from : "bikaner"
+    }
+});
